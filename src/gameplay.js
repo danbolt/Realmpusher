9f4bebf168fd6c1dac405f50f6a1fdc4b61d6657
Daniel Savage <dansvg@gmail.com>
2017-04-22 16:19:20 -0700

    Arrange rooms for puzzle

--- src/gameplay.js
+++ src/gameplay.js
@@ -1,16 +1,16 @@
 var OriginalRooms = [
-  { x: 0, y: 0 },
-  { x: 3, y: 4 },
-  { x: 2, y: 2 },
+  { x: 2, y: 1 },
   { x: 3, y: 3 },
-  { x: 4, y: 4 },
+  { x: 2, y: 2 },
   { x: 5, y: 5 },
-  { x: 6, y: 6 },
+  { x: 0, y: 2 },
+  { x: 5, y: 1 },
+  { x: 1, y: 5 },
   { x: 1, y: 0, goal: true },
   { x: 3, y: 0, goal: true },
   { x: 5, y: 2, goal: true },
-  { x: 3, y: 6, goal: true },
-  { x: 6, y: 6, goal: true },
+  { x: 3, y: 5, goal: true },
+  { x: 5, y: 5, goal: true },
 ];
 
 
