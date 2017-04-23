c9277ee30a04da53ccc9f862a8c436d754428c7c
Daniel Savage <dansvg@gmail.com>
2017-04-22 19:20:20 -0700

    Player movement

--- src/player.js
+++ src/player.js
@@ -1,8 +1,20 @@
 var Player = function(game, x, y) {
-  Phaser.Sprite.call(this, game, x, y, 'test16x16', 5);
+  Phaser.Sprite.call(this, game, x, y, 'test16x16', 13);
 
   this.walkSpeed = 100;
   this.disableMovement = false;
+
+  this.animations.add('idle_right', [21]);
+  this.animations.add('idle_down', [13]);
+  this.animations.add('idle_left', [29]);
+  this.animations.add('idle_up', [37]);
+  this.animations.add('walk_right', [22, 23], 4, true);
+  this.animations.add('walk_down', [14, 15], 4, true);
+  this.animations.add('walk_left', [30, 31], 4, true);
+  this.animations.add('walk_up', [38, 39], 4, true);
+  this.animations.play('idle_down');
+
+  this.facing = Directions.SOUTH;
 
   this.game.physics.enable(this, Phaser.Physics.ARCADE);
 };
@@ -14,16 +26,24 @@ Player.prototype.update = function () {
   {
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.RIGHT)) {
       this.body.velocity.x = this.walkSpeed;
+
+      this.facing = Directions.EAST;
     } else if (this.game.input.keyboard.isDown(Phaser.KeyCode.LEFT)) {
       this.body.velocity.x = -this.walkSpeed;
+
+      this.facing = Directions.WEST;
     } else {
       this.body.velocity.x = 0;
     }
 
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.DOWN)) {
       this.body.velocity.y = this.walkSpeed;
+
+      this.facing = Directions.SOUTH;
     } else if (this.game.input.keyboard.isDown(Phaser.KeyCode.UP)) {
       this.body.velocity.y = -this.walkSpeed;
+
+      this.facing = Directions.NORTH;
     } else {
       this.body.velocity.y = 0;
     }
@@ -32,5 +52,37 @@ Player.prototype.update = function () {
   {
     this.body.velocity.x = 0;
     this.body.velocity.y = 0;
+  }
+
+  if (this.body.velocity.x === 0 && this.body.velocity.y === 0) {
+    switch (this.facing) {
+      case Directions.EAST:
+        this.animations.play('idle_right');
+        break;
+      case Directions.SOUTH:
+        this.animations.play('idle_down');
+        break;
+      case Directions.WEST:
+        this.animations.play('idle_left');
+        break;
+      case Directions.NORTH:
+        this.animations.play('idle_up');
+        break;
+    }
+  } else {
+    switch (this.facing) {
+      case Directions.EAST:
+        this.animations.play('walk_right');
+        break;
+      case Directions.SOUTH:
+        this.animations.play('walk_down');
+        break;
+      case Directions.WEST:
+        this.animations.play('walk_left');
+        break;
+      case Directions.NORTH:
+        this.animations.play('walk_up');
+        break;
+    }
   }
 };
